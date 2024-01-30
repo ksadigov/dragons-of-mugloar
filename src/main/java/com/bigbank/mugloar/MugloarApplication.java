72e674aa065c7266770dd5db81fe60eb70b449ad
ksadigov <kamransadigov@gmail.com>
2024-01-30 21:47:04 +0200

    Initial commit

---
+++ src/main/java/com/bigbank/mugloar/MugloarApplication.java
@@ -0,0 +1,13 @@
+package com.bigbank.mugloar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MugloarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MugloarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bigbank/mugloar/MugloarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigbank.mugloar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MugloarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
