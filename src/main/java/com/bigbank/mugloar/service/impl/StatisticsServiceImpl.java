93dbc351d8c7e4dbe1884b255097dd72a112c085
ksadigov <kamransadigov@gmail.com>
2024-02-03 00:49:02 +0200

    Beautify game stats

--- src/main/java/com/bigbank/mugloar/service/impl/StatisticsServiceImpl.java
+++ src/main/java/com/bigbank/mugloar/service/impl/StatisticsServiceImpl.java
@@ -31,12 +31,14 @@ public class StatisticsServiceImpl implements StatisticsService {
 
     @Override
     public void printGameStats(List<Integer> gameScores) {
-        log.info("******************** Batch Game Statistics *****************************");
-        log.info("Game Count: " + gameScores.size());
-        log.info("Min Score: " + gameScores.stream().mapToInt(v -> v).min().orElseThrow(NoSuchElementException::new));
-        log.info("Max Score: " + gameScores.stream().mapToInt(v -> v).max().orElseThrow(NoSuchElementException::new));
-        int successGameCount = Math.toIntExact(gameScores.stream().filter(score -> score >= gameSettings.getFinalScoreThreshold()).count());
-        float successRate = (float) successGameCount * 100 / gameSettings.getRetryLimit();
+        log.info("******************** Batch Game Statistics ****************************");
+        log.info("Game Count: {}", gameScores.size());
+        long wins = gameScores.stream().filter(score -> score >= gameSettings.getFinalScoreThreshold()).count();
+        log.info("Games won: {}", wins);
+        log.info("Games lost: {}", gameScores.size() - wins);
+        log.info("Min Score: {}", gameScores.stream().mapToInt(v -> v).min().orElseThrow(NoSuchElementException::new));
+        log.info("Max Score: {}", gameScores.stream().mapToInt(v -> v).max().orElseThrow(NoSuchElementException::new));
+        float successRate = (float) wins * 100 / gameSettings.getRetryLimit();
         log.info("Success Rate: {}%", String.format("%.2f", successRate));
         log.info("***********************************************************************");
     }
