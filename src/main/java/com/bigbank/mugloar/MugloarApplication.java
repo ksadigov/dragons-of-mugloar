849a621419a10510e6a92e678fb000cfc84d7c0a
ksadigov <kamransadigov@gmail.com>
2024-02-02 17:22:43 +0200

    Implemented thread pool

--- src/main/java/com/bigbank/mugloar/MugloarApplication.java
+++ src/main/java/com/bigbank/mugloar/MugloarApplication.java
@@ -1,13 +1,56 @@
 package com.bigbank.mugloar;
 
+import com.bigbank.mugloar.config.GameProps;
+import com.bigbank.mugloar.game.GameManager;
+import com.bigbank.mugloar.service.impl.StatisticsServiceImpl;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class MugloarApplication {
+import java.util.Objects;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
-	public static void main(String[] args) {
-		SpringApplication.run(MugloarApplication.class, args);
-	}
+import static com.bigbank.mugloar.constant.Constants.GAME_SCORES;
+import static com.bigbank.mugloar.constant.Constants.INITIAL_GAME_RETRY_LIMIT;
+
+@Slf4j
+@SpringBootApplication
+@RequiredArgsConstructor
+public class MugloarApplication implements CommandLineRunner {
+    private final GameManager gameManager;
+    private final StatisticsServiceImpl statisticsService;
+    private final GameProps gameSettings;
+
+    public static void main(String[] args) {
+        SpringApplication.run(MugloarApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws InterruptedException {
+        int maxGameRuns = Objects.requireNonNullElse(gameSettings.getRetryLimit(), INITIAL_GAME_RETRY_LIMIT);
+        log.info("Starting a batch of {} games. Please wait for all results after completion.", maxGameRuns);
+
+        ExecutorService executor = Executors.newFixedThreadPool(10);
+        AtomicInteger gameNumber = new AtomicInteger(1);
+
+        for (int i = 0; i < maxGameRuns; i++) {
+            int currentGameNumber = gameNumber.getAndIncrement();
+            executor.submit(() -> gameManager.startGame(currentGameNumber));
+        }
+
+        executor.shutdown();
+        boolean finished = executor.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+
+        if (finished) {
+            statisticsService.printGameStats(GAME_SCORES);
+        } else {
+            log.error("The games did not finish within the expected time.");
+        }
+    }
 
 }
