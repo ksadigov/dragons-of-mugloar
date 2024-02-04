a0bad27ec085f5565279078d8adf468ae844223a
ksadigov <kamransadigov@gmail.com>
2024-02-04 21:55:22 +0200

    Added tests for purchaseItem and getAllShopItems methods

--- src/test/java/com/bigbank/mugloar/client/MugloarApiClientTest.java
+++ src/test/java/com/bigbank/mugloar/client/MugloarApiClientTest.java
@@ -1,13 +1,7 @@
 package com.bigbank.mugloar.client;
 
-import com.bigbank.mugloar.dto.GameStateDto;
-import com.bigbank.mugloar.dto.MessageDto;
-import com.bigbank.mugloar.dto.ReputationDto;
-import com.bigbank.mugloar.dto.TaskResultDto;
-import com.bigbank.mugloar.exception.FailedGetAllMessagesException;
-import com.bigbank.mugloar.exception.FailedRunInvestigationException;
-import com.bigbank.mugloar.exception.FailedSolveTaskException;
-import com.bigbank.mugloar.exception.FailedStartNewGameException;
+import com.bigbank.mugloar.dto.*;
+import com.bigbank.mugloar.exception.*;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -22,7 +16,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 
-import static com.bigbank.mugloar.client.MugloarApiClient.SOLVE_TASK_ENDPOINT;
+import static com.bigbank.mugloar.client.MugloarApiClient.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.*;
@@ -159,6 +153,72 @@ class MugloarApiClientTest {
         assertThatThrownBy(() -> testObj.solveTask(gameId, taskId))
                 .isInstanceOf(FailedSolveTaskException.class)
                 .hasMessageContaining("Failed to solve taskId=" + taskId + " for gameId= " + gameId);
+    }
+
+    @Test
+    void getAllShopItems_ShouldReturnListOfItems() {
+        String gameId = "gameId";
+        List<ItemDto> expectedItems = List.of(
+                new ItemDto("hpot", "Healing Potion", 50),
+                new ItemDto("cs", "Claw Sharpening", 100));
+        when(restTemplate.exchange(
+                eq(UriComponentsBuilder.fromPath(GET_ALL_SHOP_ITEMS_ENDPOINT).buildAndExpand(gameId).toString()),
+                eq(HttpMethod.GET),
+                any(),
+                any(ParameterizedTypeReference.class))
+        ).thenReturn(new ResponseEntity<>(expectedItems, HttpStatus.OK));
+
+        List<ItemDto> actualItems = testObj.getAllShopItems(gameId);
+
+        assertThat(actualItems).isNotNull().isEqualTo(expectedItems);
+    }
+
+    @Test
+    void getAllShopItems_WhenError_ShouldThrowFailedGetAllShopItemsException() {
+        String gameId = "gameId";
+        when(restTemplate.exchange(
+                eq(UriComponentsBuilder.fromPath(GET_ALL_SHOP_ITEMS_ENDPOINT).buildAndExpand(gameId).toString()),
+                eq(HttpMethod.GET),
+                any(),
+                any(ParameterizedTypeReference.class))
+        ).thenThrow(new RuntimeException("Connection error"));
+
+        assertThatThrownBy(() -> testObj.getAllShopItems(gameId))
+                .isInstanceOf(FailedGetAllShopItemsException.class)
+                .hasMessageContaining("Failed to get all shop items for gameId=" + gameId);
+    }
+
+    @Test
+    void purchaseItem_ShouldReturnPurchaseResult() {
+        String gameId = "gameId";
+        String itemId = "itemId";
+        PurchaseResultDto expectedPurchaseResult = new PurchaseResultDto(true, 1, 3, 1, 1);
+        when(restTemplate.exchange(
+                eq(UriComponentsBuilder.fromPath(PURCHASE_AN_ITEM_ENDPOINT).buildAndExpand(gameId, itemId).toString()),
+                eq(HttpMethod.POST),
+                any(),
+                eq(PurchaseResultDto.class))
+        ).thenReturn(new ResponseEntity<>(expectedPurchaseResult, HttpStatus.OK));
+
+        PurchaseResultDto actualPurchaseResult = testObj.purchaseItem(gameId, itemId);
+
+        assertThat(actualPurchaseResult).isNotNull().isEqualTo(expectedPurchaseResult);
+    }
+
+    @Test
+    void purchaseItem_WhenError_ShouldThrowFailedPurchaseItemException() {
+        String gameId = "gameId";
+        String itemId = "itemId";
+        when(restTemplate.exchange(
+                eq(UriComponentsBuilder.fromPath(PURCHASE_AN_ITEM_ENDPOINT).buildAndExpand(gameId, itemId).toString()),
+                eq(HttpMethod.POST),
+                any(),
+                eq(PurchaseResultDto.class))
+        ).thenThrow(new RuntimeException("Connection error"));
+
+        assertThatThrownBy(() -> testObj.purchaseItem(gameId, itemId))
+                .isInstanceOf(FailedPurchaseItemException.class)
+                .hasMessageContaining("Failed to purchase an itemId=" + itemId + " for gameId=" + gameId);
     }
 }
 
