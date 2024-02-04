aa4d0df89d3aee7bce3e84842e512cf50b923a9e
ksadigov <kamransadigov@gmail.com>
2024-02-04 21:36:25 +0200

    Added tests for runInvestigation method

--- src/test/java/com/bigbank/mugloar/client/MugloarApiClientTest.java
+++ src/test/java/com/bigbank/mugloar/client/MugloarApiClientTest.java
@@ -1,6 +1,8 @@
 package com.bigbank.mugloar.client;
 
 import com.bigbank.mugloar.dto.GameStateDto;
+import com.bigbank.mugloar.dto.ReputationDto;
+import com.bigbank.mugloar.exception.FailedRunInvestigationException;
 import com.bigbank.mugloar.exception.FailedStartNewGameException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,6 +12,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -24,7 +27,7 @@ class MugloarApiClientTest {
     private RestTemplate restTemplate;
 
     @InjectMocks
-    private MugloarApiClient client;
+    private MugloarApiClient testObj;
 
     @Test
     void startNewGame_ShouldReturnGameStateDto() {
@@ -36,13 +39,13 @@ class MugloarApiClientTest {
                 eq(GameStateDto.class))
         ).thenReturn(ResponseEntity.ok(expectedDto));
 
-        GameStateDto actualDto = client.startNewGame();
+        GameStateDto actualDto = testObj.startNewGame();
 
         assertThat(actualDto).isNotNull().isEqualTo(expectedDto);
     }
 
     @Test
-    void startNewGame_WhenError_ShouldThrowException() {
+    void startNewGame_WhenError_ShouldThrowFailedStartNewGameException() {
         when(restTemplate.exchange(
                 eq(MugloarApiClient.START_NEW_GAME_ENDPOINT),
                 eq(org.springframework.http.HttpMethod.POST),
@@ -50,9 +53,40 @@ class MugloarApiClientTest {
                 eq(GameStateDto.class))
         ).thenThrow(new RuntimeException("Connection error"));
 
-        assertThatThrownBy(() -> client.startNewGame())
+        assertThatThrownBy(() -> testObj.startNewGame())
                 .isInstanceOf(FailedStartNewGameException.class)
                 .hasMessageContaining("Failed to start a new game. Something went wrong.");
+    }
+
+    @Test
+    void runInvestigation_ShouldReturnReputationDto() {
+        String gameId = "gameId";
+        ReputationDto expectedDto = new ReputationDto(0, 0, 0);
+        when(restTemplate.exchange(
+                eq(UriComponentsBuilder.fromPath(MugloarApiClient.RUN_INVESTIGATION_ENDPOINT).buildAndExpand(gameId).toString()),
+                eq(org.springframework.http.HttpMethod.POST),
+                any(),
+                eq(ReputationDto.class))
+        ).thenReturn(ResponseEntity.ok(expectedDto));
+
+        ReputationDto actualDto = testObj.runInvestigation(gameId);
+
+        assertThat(actualDto).isNotNull().isEqualTo(expectedDto);
+    }
+
+    @Test
+    void runInvestigation_WhenError_ShouldThrowFailedRunInvestigationException() {
+        String gameId = "gameId";
+        when(restTemplate.exchange(
+                eq(UriComponentsBuilder.fromPath(MugloarApiClient.RUN_INVESTIGATION_ENDPOINT).buildAndExpand(gameId).toString()),
+                eq(org.springframework.http.HttpMethod.POST),
+                any(),
+                eq(ReputationDto.class))
+        ).thenThrow(new RuntimeException("Connection error"));
+
+        assertThatThrownBy(() -> testObj.runInvestigation(gameId))
+                .isInstanceOf(FailedRunInvestigationException.class)
+                .hasMessageContaining("Failed to get reputation for gameId=" + gameId);
     }
 }
 
