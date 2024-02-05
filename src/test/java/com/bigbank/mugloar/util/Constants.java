76a91d0e172c438fb0fa683fff4d79f73152c647
ksadigov <kamransadigov@gmail.com>
2024-02-05 16:13:41 +0200

    Refactored MugloarApiClientTest

---
+++ src/test/java/com/bigbank/mugloar/util/Constants.java
@@ -0,0 +1,12 @@
+package com.bigbank.mugloar.util;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class Constants {
+
+    public static final String GAME_ID = "YWU1qjHq";
+    public static final String TASK_ID = "Z4oBPt3a";
+
+}
